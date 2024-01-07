31b99a0f5106ef02f90bac5db3065b8fdda341a1
GregoryAMiller <gregorym@csu.fullerton.edu>
2024-01-07 13:50:37 -0800

    testing labeled images folder

--- label_images/label_images.py
+++ label_images/label_images.py
@@ -7,13 +7,14 @@ def main():
     base_directory = Path.cwd().parent
     images_dir = base_directory / 'images'
     labels_dir = base_directory / 'labels'
+    results_dir = Path.cwd() / 'results'
 
     # Load and configure the model
     model = label_images_functions.load_configure_model()
 
     # Walk through the images directory structure
-    for image_path in images_dir.rglob('*.png'):  # Using rglob to find .png files recursively
-        label_images_functions.label_image(model, image_path, labels_dir)
+    for image_path in images_dir.rglob('*.jpg'):  # Using rglob to find .png files recursively
+        label_images_functions.label_image(model, image_path, labels_dir, results_dir)
 
 
 if __name__ == "__main__":
