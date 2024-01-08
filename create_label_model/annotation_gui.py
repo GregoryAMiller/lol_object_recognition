d00b45b825163f1d9bb17f18e1e67cdc7a4252b8
GregoryAMiller <gregorym@csu.fullerton.edu>
2024-01-07 22:07:59 -0800

    added main for annotation gui

---
+++ create_label_model/annotation_gui.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from annotation_gui_functions import AnnotationTool
+# Main function to start the GUI
+def main():
+    root = tk.Tk()
+    # root.geometry("1000x800")  # Set the size of the window
+    default_label = 'Champion' # Set as None if you want to hand label every image
+    app = AnnotationTool(root, default_label)
+    root.mainloop()
+# # Run the main function in a separate thread
+# thread = threading.Thread(target=main)
+# thread.start()
+
+
+if __name__ == "__main__":
+    main()
