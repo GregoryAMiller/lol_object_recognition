03904f64743ec41e6af19334623d23ff526df70b
GregoryAMiller <gregorym@csu.fullerton.edu>
2024-01-15 19:29:34 -0800

    evaluate model visually

--- modeling/model_evaluation.py
+++ modeling/model_evaluation.py
@@ -4,11 +4,11 @@ from pathlib import Path
 from IPython.display import Image
 # from ultralyticsplus import render_result
 # Load the trained model
-model = YOLO(f'{Path.cwd()}/runs/detect/train3/weights/best.pt')  # Path to the trained weights
+model = YOLO(f'{Path.cwd()}/runs/detect/train6/weights/best.pt')  # Path to the trained weights
 
 # Specify the source directory containing images for detection
+# source_dir = Path(f'{Path.cwd().parent}/test/images')  # Update with your source path
 source_dir = Path(f'{Path.cwd().parent}/Ahri_Academia___Previsualizaci_n___League_of_Legends_frames')  # Update with your source path
-
 # # Results directory in the parent of script directory
 # results_dir = Path.cwd().parent / 'results'
 
@@ -18,7 +18,7 @@ source_dir = Path(f'{Path.cwd().parent}/Ahri_Academia___Previsualizaci_n___Leagu
 # Iterate over each image in the source directory
 for image_path in source_dir.glob('*.jpg'):  # Adjust for different image formats as necessary
     # Perform prediction and save the output image
-    model.predict(str(image_path), save=True, conf=0.4) # , imgsz=320
+    model.predict(str(image_path), save=True, conf=0.25) # , imgsz=320
 
     # print(f"Processed and saved results for {image_path.name} in {results_dir}")
 
